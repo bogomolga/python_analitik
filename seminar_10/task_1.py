035ab0533503102b38e4209035375c4d615e4ebf
Olga B <>
2023-02-04 17:48:06 +0300

    add home work 10

--- seminar_10/task_1.py
+++ seminar_10/task_1.py
@@ -1,34 +1,66 @@
-#1. Напишите программу, удаляющую из текста все слова, содержащие "абв". В тексте используется разделитель пробел.
+#Дана функция:
+#        f(x) = 5*x**2 + 10*x - 30
+#Определить корни
+#Найти интервалы, на которых функция возрастает
+#Найти интервалы, на которых функция убывает
+#Построить график
+#Вычислить вершину
+#Определить промежутки, на котором f > 0
+#Определить промежутки, на котором f < 0
 
-my_string = 'Питон - сабвмый лучший в миреабв язык абв'
-my_string2 = 'Питон - сабвмый лучший в миреабв язык абв'
-my_string = my_string.split()
-new_list = []
-for word in my_string:
-    if not 'абв' in word:
-        new_list.append(word)
+import numpy as np
+import matplotlib.pyplot as plt
+from sympy import *
+from sympy.plotting import plot
 
-print(' '.join(new_list)) # join - объединяет список в одну строку (тип переменной должен быть string). Объединяющим элементом будет пробел
+limit = 10
+step = 0.01
+a, b, c = 5, 10, -30
+xs = np.arange(-limit, limit, step) #последовательность
+
+def func(xs):
+    return a*xs**2 + b*xs + c  # 5*x**2 + 10*x - 30
+
+def take_roots(a, b, c):
+    discr = b**2 - 4*a*c  #дискриминант
+    if discr > 0:
+        x1 = (-b + discr**0.5)/(2*a)
+        x2 = (-b - discr**0.5)/(2*a)
+        return x1, x2
+    elif discr == 0:
+        x0 = -b/(2*a)
+        return x0, x0 #кортеж
+    else:
+        return None, None #кортеж
+print("Корни уравнения: ", take_roots(5,10,-30))
+print("Корни уравнения: ", np.roots([5,10,-30]))
+
+roots = take_roots(a, b, c)
+
+min_x, min_y = 0, 0
+min_y = min(func(xs)) #минимальный Y = -35.0
+min_x = take_roots(a, b, c - min_y)[0]  #(-1.0, -1.0)
+
+x_down = np.arange(-limit, min_x, step)
+x_up = np.arange(min_x, limit, step)
+
+x_down_positiv = np.arange(-limit, min(roots), step)
+x_down_negativ = np.arange(min(roots), min_x, step)
+x_up_negativ = np.arange(min_x, max(roots), step)
+x_up_positiv = np.arange(max(roots), limit, step)
 
 
-# Вариант 2: filter и lambda
-new_list2 = list(filter(lambda word: not 'абв' in word, my_string))
-print(' '.join(new_list2))
+plt.rcParams['lines.linestyle'] = '-'
+plt.plot(x_down_positiv, func(x_down_positiv), 'r', label='Убывание выше 0') #red
+plt.plot(x_up_positiv, func(x_up_positiv), 'b', label='Возрастание выше 0') 
+plt.rcParams['lines.linestyle'] = '--'
+plt.plot(x_down_negativ, func(x_down_negativ), 'r', label='Убывание ниже 0') #blue
+plt.plot(x_up_negativ, func(x_up_negativ), 'b', label='Возрастание ниже 0') #blue
 
+plt.plot(roots[0], func(roots[0]), 'go', label=f'Корни ({round(min(roots), 2)}, {round(max(roots), 2)})') #green o
+plt.plot(roots[1], func(roots[1]), 'go')
 
-# Вариант 3: filter и lambda
-new_list3 = list(filter(lambda word: not 'абв' in word, my_string2.split()))
-print(' '.join(new_list3))
+plt.plot(min_x, min_y, 'cx', label=f'Экстремум ({min_x}, {min_y})') #cyan x
 
-
-# Вариант 4: через list comprehensions
-new_list4 = [x for x in my_string2.split() if not 'абв' in x]
-print(' '.join(new_list4))
-
-
-# результат работы одинаковый
-
-
-
-
-
+plt.grid()
+plt.legend()
